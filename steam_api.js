3075ee862b8e31386ea006de9418de5520a0cee8
Jonathan Birkholz <rookieone@gmail.com>
2012-11-15 11:22:40 -0600

    show news

--- steam_api.js
+++ steam_api.js
@@ -5,5 +5,11 @@ SteamApi = {
     $.get(SteamApi.base + "ISteamApps/GetAppList/v0001", function(response) {
       fx(response);
     });
+  },
+  getNews: function(id, fx) {
+    console.log(id);
+    $.get(SteamApi.base + "ISteamNews/GetNewsForApp/v0002/?appid=" + id + "&count=5&maxlength=300", function(response) {
+      fx(response);
+    });
   }
 };
