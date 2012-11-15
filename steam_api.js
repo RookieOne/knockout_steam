f1fa6a1d40b0f923e3b5ddb0a1a76def4ed0523f
Jonathan Birkholz <rookieone@gmail.com>
2012-11-15 12:34:31 -0600

    search and achievements

--- steam_api.js
+++ steam_api.js
@@ -3,13 +3,31 @@ SteamApi = {
   base: "http://api.steampowered.com/",
   getAppList: function(fx) {
     $.get(SteamApi.base + "ISteamApps/GetAppList/v0001", function(response) {
-      fx(response);
+      fx(response.applist.apps.app);
     });
   },
+  searchApps: function(searchText, fx) {
+    $.get(SteamApi.base + "ISteamApps/GetAppList/v0001", function(response) {
+      var regExp = new RegExp(searchText);      
+      apps = _.filter(response.applist.apps.app, function(app) {
+        return regExp.test(app.name);
+      });
+      fx(apps);
+    });    
+  },
   getNews: function(id, fx) {
-    console.log(id);
     $.get(SteamApi.base + "ISteamNews/GetNewsForApp/v0002/?appid=" + id + "&count=5&maxlength=300", function(response) {
-      fx(response);
+      fx(response.appnews.newsitems);
     });
-  }
+  },
+  getCurrentPlayers: function(id, fx) {
+    $.get(SteamApi.base + "ISteamUserStats/GetNumberOfCurrentPlayers/v0001/?appid=" + id, function(response) {      
+      fx(response.response);
+    });
+  },
+  getAchievements: function(id, fx) {
+    $.get(SteamApi.base + "ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=" + id + "&xml=1", function(response) {      
+      fx(response.achievementpercentages.achievements);
+    });
+  }  
 };
